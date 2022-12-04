45b6d456f7417a6593c5cdce6185394bd31a9865
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-12-04 05:09:20 +0300

    One to one Rlshp

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,5 @@
 class Author < ApplicationRecord
     has_many :posts
+
+    has_one :profile
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ApplicationRecord
     #Each Post is associated with one Author
-    belongs_to :author 
+    belongs_to :author   #Gives us access to an author method in our Post class. Retrieve them  by using post = Post.first // post.author 
 end
